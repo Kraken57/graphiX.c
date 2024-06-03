4011edd6dc64ed9658a3dabc0523f9c8d6c0c2b0
Kraken57 <kasaad1609@gmail.com>
2024-06-03 21:09:36 +0530

    BACKGROUND_COLOR FIX

--- mango/example.c
+++ mango/example.c
@@ -10,41 +10,26 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-void mangoc_fill_rect(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
-	int x0, int y0, size_t w, size_t h, uint32_t color)
-{
-	for (int dy = 0; dy < (int)h; ++dy) {
-		int y = y0 + dy;
-		if (0 <= y && y < (int)pixels_height) {
-			for (int dx = 0; dx < (int)w; ++dx) {
-				int x = x0 + dx;
-				if (0 <= x && x < (int)pixels_width) {
-					pixels[y * pixels_width + x] = color;
-				}
-			}
-		}
-	}
-}
-
-static uint32_t pixels[HEIGHT*WIDTH];
-
 #define COLS 8 
 #define ROWS 6
 #define CELL_WIDTH (WIDTH/COLS)
 #define CELL_HEIGHT (HEIGHT/ROWS)
 
+#define BACKGROUND_COLOR 0xFF202020
+
+static uint32_t pixels[HEIGHT*WIDTH];
+
+
+
 int main(void)
 {
-	mangoc_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
+	mangoc_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
 	for (int y = 0; y < ROWS; ++y) {
 		for (int x = 0; x < COLS; ++x) {
-			uint32_t color;
+			uint32_t color = BACKGROUND_COLOR;
 			if ((x + y) % 2 == 0) {
-				color = 0xFF0000FF;
-			}
-			else {
-				color = 0xFF00FF00;
+				color = 0xFF2020FF;
 			}
 			mangoc_fill_rect(pixels, WIDTH, HEIGHT, x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
 		}
@@ -52,7 +37,7 @@ int main(void)
 
 
 
-	const char* file_path = "output.ppm";
+	const char* file_path = "checker_example.ppm";
 	char err_buf[256];
 	Errno err = mangoc_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
 	if (err) {

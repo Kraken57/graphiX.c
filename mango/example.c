66f8ffea9d054981c335dd76b95532746da0fa42
Kraken57 <kasaad1609@gmail.com>
2024-06-03 21:53:52 +0530

    Generating circle

--- mango/example.c
+++ mango/example.c
@@ -15,13 +15,34 @@
 #define CELL_WIDTH (WIDTH/COLS)
 #define CELL_HEIGHT (HEIGHT/ROWS)
 
-#define BACKGROUND_COLOR 0xFF202020
+#define BACKGROUND_COLOR 0xFFFFFFFF //0xFF202020
+#define FOREGROUND_COLOR 0xFF2020FF
 
 static uint32_t pixels[HEIGHT*WIDTH];
 
+void mangoc_fill_circle(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
+	int cx, int cy, int r, uint32_t color)
+{
+	int x1 = cx - r;
+	int y1 = cy - r;
+	int x2 = cx + r;
+	int y2 = cy + r;
+	for (int y = y1; y <= y2; ++y) {
+		if (0 <= y && y < (int)pixels_height) {
+			for (int x = x1; x <= x2; ++x) {
+				if (0 <= x && x < (int)pixels_width) {
+					int dx = x - cx;
+					int dy = y - cy;
+					if (dx * dx + dy * dy <= r * r) {
+						pixels[y * pixels_width + x] = color;
+					}
+				}
+			}
+		}
+	}
+}
 
-
-int main(void)
+bool checker_example(void)
 {
 	mangoc_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
@@ -37,14 +58,42 @@ int main(void)
 
 
 
-	const char* file_path = "checker_example.ppm";
+	const char* file_path = "checker.ppm";
 	char err_buf[256];
 	Errno err = mangoc_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
 	if (err) {
 		strerror_s(err_buf, sizeof(err_buf), err);
 		fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, err_buf);
-		return 1;
+		return false;
 	}
+	return true;
+}
+
+bool circle_example(void)
+{
+	mangoc_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+	mangoc_fill_circle(pixels, WIDTH, HEIGHT,
+		WIDTH/2, HEIGHT/2, 100,
+		FOREGROUND_COLOR);
+
+	const char* file_path = "circle.ppm";
+	char err_buf[256];
+	Errno err = mangoc_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+	if (err) {
+		strerror_s(err_buf, sizeof(err_buf), err);
+		fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, err_buf);
+		return false;
+	}
+	return true;
+	return 0;
+}
+
+int main(void)
+{
+	if (!checker_example()) return -1;
+	if (!circle_example()) return -1;
+
 	return 0;
 
 }
--- mango/mango.h
+++ mango/mango.h
@@ -10,8 +10,8 @@
 void mangoc_fill(uint32_t* pixels, size_t width, size_t height, uint32_t color);
 void mangoc_fill_rect(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
     int x0, int y0, size_t w, size_t h, uint32_t color);
-//void mangoc_fill_circle(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
-//    int cx, int cy, int r, uint32_t color);
+void mangoc_fill_circle(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
+    int cx, int cy, int r, uint32_t color);
 typedef int Errno;
 Errno mangoc_save_to_ppm_file(uint32_t* pixels, size_t width, size_t height, const char* file_path);
 

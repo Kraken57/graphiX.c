3dc3fe484e420215dfdf46a5155865ac91a3f3dc
Kraken57 <kasaad1609@gmail.com>
2024-06-09 21:36:56 +0530

    Implemented lines logic

--- mango/example.c
+++ mango/example.c
@@ -10,8 +10,8 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-#define COLS (8*3)
-#define ROWS (6*3)
+#define COLS (8*2)
+#define ROWS (6*2)
 #define CELL_WIDTH (WIDTH/COLS)
 #define CELL_HEIGHT (HEIGHT/ROWS)
 
@@ -20,27 +20,57 @@
 
 static uint32_t pixels[HEIGHT*WIDTH];
 
-void mangoc_fill_circle(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
-	int cx, int cy, int r, uint32_t color)
+void swap_int(int* a, int* b)
 {
-	int x1 = cx - r;
-	int y1 = cy - r;
-	int x2 = cx + r;
-	int y2 = cy + r;
-	for (int y = y1; y <= y2; ++y) {
-		if (0 <= y && y < (int)pixels_height) {
-			for (int x = x1; x <= x2; ++x) {
-				if (0 <= x && x < (int)pixels_width) {
-					int dx = x - cx;
-					int dy = y - cy;
-					if (dx * dx + dy * dy <= r * r) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void mangoc_draw_line(uint32_t* pixels, size_t pixels_width, size_t pixels_height,
+	int x1, int y1, int x2, int y2,
+	uint32_t color)
+{
+	// y = k*x + c
+	//let y1= k*x1 + c and y2 = k*x2 + c
+	//Rearranging we get 
+	// y1 - k*x1 = c and (y2 - y1)/(x2 - x1) = k
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+	if (dx != 0) {
+		int c = y1 - dy * x1 / dx;
+
+		if (x1 > x2) swap_int(&x1, &x2);
+		for (int x = x1; x <= x2; ++x) {
+			if (0 <= x && x < (int)pixels_width) {
+				int sy1 = dy * x / dx + c;
+				int sy2 = dy * (x + 1) / dx + c;
+				if (sy1 > sy2) swap_int(&sy1, &sy2);
+				for (int y = sy1; y <= sy2; ++y) {
+					if (0 <= y && y < (int)pixels_height) {
 						pixels[y * pixels_width + x] = color;
 					}
 				}
 			}
 		}
+	
+	
+	}
+	else {
+		int x = x1;
+		if (0 <= x && x < (int)pixels_width) {
+			if (y1 > y2) swap_int(&y1, &y2);
+			for (int y = y1; y <= y2; ++y) {
+				if (0 <= y && y < (int)pixels_height) {
+					pixels[y * pixels_width + x] = color;
+				}
+			}
+		}
 	}
 }
+
 
 bool checker_example(void)
 {
@@ -106,10 +136,64 @@ bool circle_example(void)
 	return 0;
 }
 
+bool lines_example(void)
+{
+	mangoc_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		0, 0, WIDTH, HEIGHT,
+		FOREGROUND_COLOR);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH, 0, 0, HEIGHT,
+		FOREGROUND_COLOR);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		0, 0, WIDTH / 4, HEIGHT,
+		0xFF20FF20);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH / 4, 0, 0, HEIGHT,
+		0xFF20FF20);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH / 2, 0, WIDTH, HEIGHT,
+		0xFF20FF20);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH, 0, WIDTH / 4 * 3, HEIGHT,
+		0xFF20FF20);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH / 4 * 3, 0, WIDTH, HEIGHT,
+		0xFF20FF20);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		0, HEIGHT / 2, WIDTH, HEIGHT / 2,
+		0xFFFF3030);
+
+	mangoc_draw_line(pixels, WIDTH, HEIGHT,
+		WIDTH / 2, 0, WIDTH / 2, HEIGHT,
+		0xFFFF3030);
+
+	const char* file_path = "lines.ppm";
+	char err_buf[256];
+	Errno err = mangoc_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+	if (err) {
+		strerror_s(err_buf, sizeof(err_buf), err);
+		fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, err_buf);
+		return false;
+	}
+
+	return true;
+
+}
+
 int main(void)
 {
 	if (!checker_example()) return -1;
 	if (!circle_example()) return -1;
+	if (!lines_example()) return -1;
 
 	return 0;
 
